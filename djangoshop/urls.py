21423a16a9c34b9c1c25b83e3de1a87650a35382
DepStudy <pystudy@mail.ru>
2020-10-08 14:27:23 +0300

    Lesson 3. Django ORM start

--- djangoshop/urls.py
+++ djangoshop/urls.py
@@ -1,7 +1,11 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
+
 import mainapp.views as mainapp
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -15,3 +19,7 @@ urlpatterns = [
     path("contacts/", mainapp.contacts, name="contacts"),
     path("details/", mainapp.details, name="details"),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
