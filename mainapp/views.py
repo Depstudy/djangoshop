2b0a05609956a91ffe3f45fa1cc0f3cf9616fd4b
Depstudy <60187740+Depstudy@users.noreply.github.com>
2020-10-11 11:12:12 +0300

    Lesson 3 (#3)
    * Lesson 3. Django ORM start
    
    * Lesson 3. Models in context
    
    * Lesson 3. Urls for app

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,66 +1,32 @@
-import datetime
-
+from django.conf import settings
 from django.shortcuts import render
+from django.utils import timezone
+
+from .models import Product, ProductCategory
 
 
 def main(request):
     title = "Interior"
-    products = [
-        {
-            "class_prod": "tab-product-1",
-            "class_base": "tab-product",
-            "image_src": "tab-products/product_1.jpg",
-            "image_href": "#",
-            "alt": "product-1",
-        },
-        {
-            "class_base": "tab-product",
-            "image_src": "tab-products/product_2.jpg",
-            "image_href": "#",
-            "alt": "product-2",
-        },
-        {
-            "class_base": "tab-product",
-            "image_src": "tab-products/product_3.jpg",
-            "image_href": "#",
-            "alt": "product-3",
-        },
-        {
-            "class_prod": "tab-product-4",
-            "class_base": "tab-product",
-            "image_src": "tab-products/product_4.jpg",
-            "image_href": "#",
-            "alt": "product-4",
-        },
-    ]
-    content = {"title": title, "products": products}
+
+    products = Product.objects.all()
+    
+    content = {"title": title, "products": products, "media_url": settings.MEDIA_URL}
     return render(request, "mainapp/index.html", content)
 
 
-def products(request):
+def products(request, pk=None):
     title = "Interior Products"
-    links_menu = [
-        {"href": "products_all", "name": "ALL", "class": "main-menu-link"},
-        {"href": "products_home", "name": "HOME", "class": "main-menu-link"},
-        {"href": "products_office", "name": "OFFICE", "class": "main-menu-link"},
-        {"href": "products_modern", "name": "MODERN", "class": "main-menu-link"},
-        {"href": "products_classic", "name": "CLASSIC", "class": "main-menu-link"},
-    ]
-    same_products = [
-        {"image_src": "products/product_1.jpg", "alt": "product_1", "class_base": "main-product"},
-        {"image_src": "products/product_2.jpg", "alt": "product_2", "class_base": "main-product", "class_prod": "main-product-2"},
-        {"image_src": "products/product_3.jpg", "alt": "product_3", "class_base": "main-product"},
-        {"image_src": "products/product_4.jpg", "alt": "product_4", "class_base": "main-product"},
-        {"image_src": "products/product_5.jpg", "alt": "product_5", "class_base": "main-product", "class_prod": "main-product-5"},
-        {"image_src": "products/product_6.jpg", "alt": "product_6", "class_base": "main-product"},
-    ]
-    content = {"title": title, "links_menu": links_menu, "same_products": same_products}
+    links_menu = ProductCategory.objects.all()
+    same_products = Product.objects.all()
+    content = {"title": title, "links_menu": links_menu, "same_products": same_products, "media_url": settings.MEDIA_URL,}
+    if pk:
+        print(f"User select category: {pk}")
     return render(request, "mainapp/products.html", content)
 
 
 def contacts(request):
     title = "Interior Contacts"
-    visit_date = datetime.datetime.now()
+    visit_date = timezone.now()
     locations = [
         {"city": "Москва", "phone": "+7-888-888-8888", "email": "info@interior.ru", "address": "В пределах МКАД"},
         {"city": "Екатеринбург", "phone": "+7-777-777-7777", "email": "info_yekaterinburg@interior.ru", "address": "Близко к центру",},
